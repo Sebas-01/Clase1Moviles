6ddfcd36fe2d022059a2ae86b7f59a62acb05249
Sebas-01 <128066214+Sebas-01@users.noreply.github.com>
2024-02-15 08:19:50 -0500

    part1-clase3

---
+++ app/src/main/java/co/nelson/appuno/Pantalla2.java
@@ -0,0 +1,14 @@
+package co.nelson.appuno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Pantalla2 extends AppCompatActivity {
+
+   
+}
